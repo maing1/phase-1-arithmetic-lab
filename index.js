a3b1998a7ccd1e7d89ee611624ab7fe3a8343a11
maing1 <faithmaingi1622@gmail.com>
2024-09-24 10:40:52 +0300

    first command

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4
+let max = Math.max(20, 10, 9);
